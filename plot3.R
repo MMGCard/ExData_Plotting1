36b13c97ac8d9964c88b7221984fd11518f76970
Marc <32173658+MMGCard@users.noreply.github.com>
2018-03-01 13:57:49 -0600

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,29 @@
+# Coursera Exploratory Data Analysis - Project 1
+# Marcelo Cardoso
+
+setwd("C:/Users/mmurta/Documents/Coursera/Exploratory Data")
+
+# Rough estimate of memory requirement for a dataset with 2,075,259 rows and 9 columns: 
+# 2075259 * 9 * 8 = 0.1425 GB
+
+HsHldPwr <- read.table("~/Coursera/Exploratory Data/household_power_consumption.txt", header = TRUE, sep = ";", colClasses = "character")
+
+HsHldPwr$Date <- as.Date(HsHldPwr$Date, "%d/%m/%Y")
+HsHldPwr <- subset(HsHldPwr, (as.Date("2007-01-31") < Date) & (Date < as.Date("2007-02-03")))
+
+HsHldPwr$Time <- strptime(paste(HsHldPwr$Date,HsHldPwr$Time), "%Y-%m-%d %H:%M:%S")
+HsHldPwr$Global_active_power <- as.numeric(HsHldPwr$Global_active_power)
+HsHldPwr$Global_reactive_power <- as.numeric(HsHldPwr$Global_reactive_power)
+HsHldPwr$Voltage <- as.numeric(HsHldPwr$Voltage)
+HsHldPwr$Global_intensity <- as.numeric(HsHldPwr$Global_intensity)
+HsHldPwr$Sub_metering_1 <- as.numeric(HsHldPwr$Sub_metering_1)
+HsHldPwr$Sub_metering_2 <- as.numeric(HsHldPwr$Sub_metering_2)
+HsHldPwr$Sub_metering_3 <- as.numeric(HsHldPwr$Sub_metering_3)
+
+# Plot 3
+png(filename = "plot3.png", width = 480, height = 480)
+plot(HsHldPwr$Time, HsHldPwr$Sub_metering_1, type = "l", xlab = "", ylab = "Energy Sub Metering")
+points(HsHldPwr$Time, HsHldPwr$Sub_metering_2, type = "l", col = "red", xlab = "")
+points(HsHldPwr$Time, HsHldPwr$Sub_metering_3, type = "l", col = "blue", xlab = "")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
